c3ef9f57e43b4307d6582bad637cf25a49bc66f0
Clarence Ho <clarenceho@gmail.com>
2024-06-08 20:27:03 -0700

    WIP single datasource

---
+++ src/main/java/net/clarenceho/multi_liquibase/MultiLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package net.clarenceho.multi_liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiLiquibaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiLiquibaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/clarenceho/multi_liquibase/MultiLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package net.clarenceho.multi_liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiLiquibaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
